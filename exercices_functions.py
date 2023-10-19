e1a4e9214a412b4a3ae665cd2545616b14c60de8
Ziad Chamrah <147912275+Chamrah@users.noreply.github.com>
2023-10-19 07:43:05 +0100

    Add files via upload

---
+++ exercices_functions.py
@@ -0,0 +1,41 @@
+# Q1
+n=input("Entrer des characteres : ")
+print(len(n))
+# Q2
+n=input("Entrer des characteres : ")
+print()
+# Q3
+n3="Barca"
+print(n3.replace("Barca","Real madrid"))
+# Q4
+n4="Marrakech is the best city"
+print(n4.find("is"))
+# Q5
+n4="Hel lo"
+print(n4.replace(" ",""))
+
+#Q6
+a=str(input())
+if (a==a[::-1]):
+    print("C'est un palindrome")
+else:
+    print("Ce n'est pas un palidrome")
+# #Q7
+n7="Ziad"
+print(n7.upper())
+
+#Q8
+n8="ahmed and"
+print(n8.count("a"))
+
+# Q9
+n9="Charactere"
+print(n9.replace(n9,"deuxieme charactere"))
+
+# Q10
+n10="Bonjour DEV 104 ISGI"
+print(n10.split())
+
+
+
+
